d76aea7627a959a7864a8b6238c0121e72c2b74f
EJ Cordero <cordero.ellainejoy.d@gmail.com>
2025-10-08 00:25:12 +0800

    First Commit

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,8 +44,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 //$router->get('/', 'Welcome::index');
-$router->get('/', 'UsersController::index');
+$router->get('/', 'LoginController::index');
 $router->get('/users', 'UsersController::index');
 $router->match('/users/create', 'UsersController::create', ['GET', 'POST']);
 $router->match('/users/update/{id}', 'UsersController::update', ['GET', 'POST']);
 $router->get('/users/delete/{id}', 'UsersController::delete');
+
+$router->match('/login', 'LoginController::index', ['GET', 'POST']);
+$router->get('/logout', 'LoginController::logout');
